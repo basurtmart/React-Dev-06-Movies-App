a796a381af56afb93c8d338d389d9cb0091d960c
mbasurto <mbd.97@hotmail.com>
2026-02-14 21:57:48 -0600

    feat: enhance HomeScreen with loading indicator and safe area insets

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,16 +1,27 @@
 import { useMovies } from '@/presentation/hooks/useMovies';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
+    const safeArea = useSafeAreaInsets();
     const { nowPlayingQuery } = useMovies();
 
+    if (nowPlayingQuery.isLoading) {
+        return (
+            <View className='justify-center items-center flex-1'>
+                <ActivityIndicator
+                    color='purple'
+                    size={40} />
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
-            <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+        <View className='mt-2' style={{ paddingTop: safeArea.top }}>
+            <Text className='text-3xl font-bold px-4 mb-2'>Home Screen</Text>
         </View>
-    )
+    );
 }
 
 export default HomeScreen;
