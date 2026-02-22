0901bc6115939b921d33f61648448acf0fa440b3
mbasurto <mbd.97@hotmail.com>
2026-02-22 15:46:39 -0600

    feat: enhance movie data structure and implement movie fetching by ID

---
+++ core/actions/movie/get-movie-by-id.action.ts
@@ -0,0 +1,18 @@
+import { movieApi } from "@/core/api/movie-api";
+import { CompleteMovie } from "@/infrastructure/interfaces/movie.interface";
+import { MovieDBMovieReponse } from "@/infrastructure/interfaces/moviedb-movie.response";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
+
+export const getMovieByIdAction = async (id: number | string): Promise<CompleteMovie> => {
+    try {
+        const { data } = await movieApi.get<MovieDBMovieReponse>(`/${id}`);
+
+        // console.log('Raw movie data from API:', JSON.stringify(data, null, 2));
+        const movie = MovieMapper.fromTheMovieDBMovieToCompleteMovie(data);
+
+        return movie;
+    } catch (error) {
+        console.error('Error fetching movie by ID:', error);
+        throw 'Failed to fetch movie by ID';
+    }
+}
--- presentation/components/movies/MoviePoster.tsx
+++ presentation/components/movies/MoviePoster.tsx
@@ -1,3 +1,4 @@
+import { router } from 'expo-router';
 import React from 'react';
 import { Image, Pressable } from 'react-native';
 
@@ -10,7 +11,9 @@ interface Props {
 
 const MoviePoster = ({ id, poster, smallPoster = false, className }: Props) => {
     return (
-        <Pressable className={`active:opacity-90 px-2 ${className || ''}`}>
+        <Pressable
+            className={`active:opacity-90 px-2 ${className || ''}`}
+            onPress={() => router.push(`/movie/${id}`)}>
             <Image
                 source={{ uri: poster }}
                 className="shadow-lg rounded-2xl w-full h-full"
