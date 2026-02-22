71512bc5b173daad7173606e22747498ae7dda50
mbasurto <mbd.97@hotmail.com>
2026-02-22 16:03:58 -0600

    feat: implement useMovie hook for fetching movie data by ID and update MovieScreen component

--- core/actions/movie/get-movie-by-id.action.ts
+++ core/actions/movie/get-movie-by-id.action.ts
@@ -8,6 +8,7 @@ export const getMovieByIdAction = async (id: number | string): Promise<CompleteM
         const { data } = await movieApi.get<MovieDBMovieReponse>(`/${id}`);
 
         // console.log('Raw movie data from API:', JSON.stringify(data, null, 2));
+        console.log('Película - HTTP cargada');
         const movie = MovieMapper.fromTheMovieDBMovieToCompleteMovie(data);
 
         return movie;
